f232fd4cd80669619683c6c88e9237e5e6cacca2
liub <17717808642@163.com>
2022-09-14 18:09:20 +0800

    base修改

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   description: '常用的API封装，依据ElementUI封装全局组件',
   // 打包目录
   outDir: '../dest',
-  base: './',
+  base: '/base/',
   head: [
     // 添加图标
     ['link', { rel: 'icon', href: '/favicon.ico' }],
