65dc96c0e6e096d88520e6cdae4877c1cca974e1
liub <17717808642@163.com>
2022-09-14 15:58:37 +0800

    new

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,56 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  title: 'front-endLiu',
+  description: '常用的API封装，依据ElementUI封装全局组件',
+  // 打包目录
+  dest: './dist',
+  head: [
+    // 添加图标
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+  ],
+  plugins: [],
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Utils', link: '/utils/' },
+      { text: 'Api', link: '/api/' },
+      { text: 'Component', link: '/component/' },
+      { text: 'Blog', link: 'https://blog.csdn.net/chen548246' },
+      { text: 'Github', link: 'https://github.com/front-endLiu' },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'front-endLiu © 2022-present',
+    },
+    sidebar: {
+      '/utils/': [
+        {
+          text: '工具类',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            { text: '工具', link: '/utils/' },
+            { text: '防抖', link: '/utils/debounce' },
+            { text: '节流', link: '/utils/throttle' },
+          ],
+        },
+        {
+          text: '导出类',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            { text: '防抖', link: '/utils/debounce' },
+            { text: '节流', link: '/utils/throttle' },
+          ],
+        },
+      ],
+      '/api/': [
+        {
+          text: 'API封装',
+          items: [],
+        },
+      ],
+    },
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,34 @@
+---
+layout: home
+
+hero:
+  name: front-endLiu
+  text: utils api component in element-ui
+  tagline: use front-end web platform
+  actions:
+    - theme: brand
+      text: utils
+      link: /utils/debounce
+    - theme: brand
+      text: api
+      link: /api/index
+    - theme: brand
+      text: component
+      link: /component/index
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/front-endLiu
+features:
+  - icon: 💴
+    title: Vite, The DX that can't be beat
+    details: Lorem ipsum...
+  - icon: 💵
+    title: Power of Vue meets Markdown
+    details: Lorem ipsum...
+  - icon: 💶
+    title: Simple and minimal, always
+    details: Lorem ipsum...
+  - icon: 💶
+    title: Simple and minimal, always
+    details: Lorem ipsum...
+---
