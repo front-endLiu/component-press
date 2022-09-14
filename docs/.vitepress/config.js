78dfa39935bc3a7a37cf59c9df7b599b82efe690
liub <17717808642@163.com>
2022-09-14 17:54:46 +0800

    修改编译

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   title: 'front-endLiu',
   description: '常用的API封装，依据ElementUI封装全局组件',
   // 打包目录
-  dest: './dist',
+  outDir: '../dest',
   base: './',
   head: [
     // 添加图标
